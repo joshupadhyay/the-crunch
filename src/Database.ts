9562434061a203b9eb6de008294e7058568ec0c1
user.email <joshupad@gmail.com>
2026-02-11 10:56:54 -0500

    created database interface and implementation with local map storage

--- src/Database.ts
+++ src/Database.ts
@@ -1,3 +1,50 @@
 import type { MessageParam } from "@anthropic-ai/sdk/resources";
+import { randomUUIDv7 } from "bun";
 
 export const CHATDATABASE: Map<string, MessageParam[]> = new Map();
+
+export interface DatabaseInterface {
+  createConversation(): string;
+
+  getConversation(id: string): MessageParam[];
+
+  pushMessage(
+    id: string,
+    role: "user" | "assistant",
+    message: string,
+  ): MessageParam[];
+
+  // this is how you specify optional function in interface
+  deleteConversation?(id: string): void;
+}
+
+export class LocalStorage implements DatabaseInterface {
+  CHATDATABASE: Map<string, MessageParam[]> = new Map();
+
+  createConversation(): string {
+    const newConversationID = randomUUIDv7().toString();
+    CHATDATABASE.set(newConversationID, []);
+    return newConversationID;
+  }
+
+  getConversation(id: string) {
+    const messages = CHATDATABASE.get(id);
+
+    if (!messages) {
+      throw new Error(`Conversation ${messages} does not exist.`);
+    }
+    return messages;
+  }
+
+  pushMessage(
+    id: string,
+    role: "user" | "assistant",
+    message: string,
+  ): MessageParam[] {
+    const messages = this.getConversation(id);
+
+    messages?.push({ role: role, content: message });
+
+    return messages;
+  }
+}
